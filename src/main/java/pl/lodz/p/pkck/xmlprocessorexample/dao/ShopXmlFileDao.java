246b38deafbecbc344ec169b899a7d4332417baf
tigeromichal <empiasecki96@gmail.com>
2018-06-11 23:41:41 +0200

    Implemented ShopXmlFileDao

--- src/main/java/pl/lodz/p/pkck/xmlprocessorexample/dao/ShopXmlFileDao.java
+++ src/main/java/pl/lodz/p/pkck/xmlprocessorexample/dao/ShopXmlFileDao.java
@@ -1,17 +1,42 @@
 package pl.lodz.p.pkck.xmlprocessorexample.dao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.lodz.p.pkck.xmlprocessorexample.model.Shop;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
 
 public class ShopXmlFileDao implements Dao<Shop> {
 
+    Logger log = LoggerFactory.getLogger(ShopXmlFileDao.class);
+
     @Override
     public Shop read(String path) {
-        return null;
+        Shop shop = null;
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(Shop.class);
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            shop = (Shop) jaxbUnmarshaller.unmarshal(new File("./" + path));
+        } catch (JAXBException e) {
+            log.error(e.getMessage(), e);
+        }
+        return shop;
     }
 
     @Override
     public void write(Shop obj, String path) {
-
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(Shop.class);
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            jaxbMarshaller.marshal(obj, new File("./" + path));
+        } catch (JAXBException e) {
+            log.error(e.getMessage(), e);
+        }
     }
 
 }
