7420efc4a792e70445f76d491900aa5c70602f3e
tigeromichal <empiasecki96@gmail.com>
2018-06-11 23:42:25 +0200

    Code style

--- src/main/java/pl/lodz/p/pkck/xmlprocessorexample/model/Offer.java
+++ src/main/java/pl/lodz/p/pkck/xmlprocessorexample/model/Offer.java
@@ -1,6 +1,9 @@
 package pl.lodz.p.pkck.xmlprocessorexample.model;
 
-import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 
 @XmlRootElement(name = "oferta")
