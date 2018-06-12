206f9de15204a6de5ab1f7c2e346dab2b04c72a3
tigeromichal <empiasecki96@gmail.com>
2018-06-12 11:44:27 +0200

    Reformated fxml view file ; Added fx:ids

--- src/main/java/pl/lodz/p/pkck/xmlprocessorexample/controller/MenuController.java
+++ src/main/java/pl/lodz/p/pkck/xmlprocessorexample/controller/MenuController.java
@@ -3,6 +3,7 @@ package pl.lodz.p.pkck.xmlprocessorexample.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.lodz.p.pkck.xmlprocessorexample.dao.ShopXmlFileDao;
+import pl.lodz.p.pkck.xmlprocessorexample.model.Shop;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,10 +12,16 @@ public class MenuController extends Controller {
 
     private static final Logger logger = LoggerFactory.getLogger(MenuController.class);
 
+    private Shop shop;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    private void loadXml() {
         ShopXmlFileDao dao = new ShopXmlFileDao();
-        dao.read("/src/main/resources/xml/zamówienia.xml");
+        shop = dao.read("/src/main/resources/xml/zamówienia.xml");
     }
 
 }
